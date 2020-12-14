0400cd7b98873b13d13338afcae4143d192a9a83
John Kang <john@hphk.kr>
2020-12-15 00:27:51 +0900

    Add brewfiles & mountpint

---
+++ brewfiles/common.Brewfile
@@ -0,0 +1,32 @@
+cask_args appdir: "/Applications"
+
+tap "homebrew/cask"
+brew "mas"
+
+cask "alfred"
+cask "shiftit"
+
+cask "google-chrome"
+cask "firefox"
+
+cask "dropbox"
+cask "google-backup-and-sync"
+
+cask "zoom"
+
+cask "spotify"
+cask "vlc"
+
+cask "1password"
+cask "1password-cli"
+
+cask "typora"
+cask "visual-studio-code"
+
+cask "tunnelbear"
+
+mas "KakaoTalk", id: 869223134
+mas "com.alice.mac.GetPlainText", id: 508368068
+mas "Notability", id: 736189492
+mas "Amphetamine", id: 937984704
+mas "Bandizip", id: 1265704574
---
+++ caskconfig.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# must have
+brew cask install alfred
+
+# browsers
+brew cask install google-chrome
+brew cask install firefox
+
+# cloud drive
+brew cask install dropbox
+brew cask install google-backup-and-sync
+
+# editor
+brew cask install iterm2
+# brew cask install atom
+brew cask install sublime-text
+brew cask install visual-studio-code
+
+# dev
+brew cask install sketch
+
+# virtual machine
+brew cask install virtualbox
+brew cask install vagrant
+
+# work-related
+# brew cask install wunderlist
+brew cask install evernote
+brew cask install polymail
+
+# msg
+brew cask install telegram
+brew cask install slack
+brew cask install skype
+brew cask install dayone-cli
+
+# player
+brew cask install vlc
+
+# hobbies
+brew cask install logic
+brew cask install seashore
+brew cask install tunnelbear
+
+# misc
+brew cask install transmission
+brew cask install shiftit
+brew cask install cheatsheet
+brew cask install easyfind
